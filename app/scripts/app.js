ab0d14132e7b46d7d85a24a247aa3cb266bc32a4
BoN4T <bonat_cdz@hotmail.com>
2014-12-27 17:24:38 +0100

    Complete directories

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yeomanProjectsApp
+ * @description
+ * # yeomanProjectsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yeomanProjectsApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
